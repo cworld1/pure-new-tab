3b81f24692f1fd1e1b55cb7c1d4d2cbed919fcc1
CWorld <cworld0@qq.com>
2024-07-03 14:26:06 +0800

    refactor: remove unused containerElement in HitokotoComponent

--- src/components/hitokoto.ts
+++ src/components/hitokoto.ts
@@ -3,7 +3,6 @@ import browser from "webextension-polyfill";
 export default class HitokotoComponent extends HTMLElement {
   hitokotoElement!: HTMLElement;
   fromElement!: HTMLElement;
-  containerElement!: HTMLElement;
 
   constructor() {
     super();
@@ -13,16 +12,15 @@ export default class HitokotoComponent extends HTMLElement {
     browser.runtime.sendMessage("getHitokoto").then((response) => {
       this.hitokotoElement.innerText = response.hitokoto ?? "Loading failed";
       this.fromElement.innerText = response.from ?? "Loading failed";
-      this.containerElement.style.opacity = "1";
     });
   };
 
   connectedCallback() {
     this.hitokotoElement = this.querySelector(".hitokoto") as HTMLElement;
     this.fromElement = this.querySelector(".from") as HTMLElement;
-    this.containerElement = this.querySelector(".container") as HTMLElement;
 
     this.loadHitokoto();
-    this.containerElement.addEventListener("click", this.loadHitokoto);
+    this.addEventListener("click", this.loadHitokoto);
+    this.style.opacity = "1";
   }
 }
